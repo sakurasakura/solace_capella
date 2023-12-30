60e758df4dcb244b38f5720a8c9345209c865808
sakurasakura <khavy.january@gmail.com>
2023-12-30 22:56:51 +0700

    add waiting message

--- src/controller/schedule_controller.js
+++ src/controller/schedule_controller.js
@@ -14,24 +14,27 @@ const messageSchedule = (user, message) => {
 };
 // !c today
 const messageTodaySchedule = async (user, message) => {
+  const msg = await message.reply("Chờ mình tí nhé...");
   const data = await ScheduleRepo.getTodaySchedule(user, message);
   //console.log("in message");
- // console.log(data);
+  // console.log(data);
   if (!data || data.length === 0) {
-    message.reply("Hôm nay lịch trống!");
+    //message.reply("Hôm nay lịch trống!");
+    msg.edit("Hôm nay lịch trống!");
     return;
   }
   var str = "";
   data.forEach((element) => {
     str += element.toString();
   });
-  message.reply(str);
+  //message.reply(str);
+  msg.edit(str);
 };
 // !c tomorrow
 const messageTomorrowSchedule = async (user, message) => {
   const data = await ScheduleRepo.getTomorrowSchedule(user, message);
- // console.log("in message");
- // console.log(data);
+  // console.log("in message");
+  // console.log(data);
   if (!data || data.length === 0) {
     message.reply("Ngày mai lịch trống!");
     return;
@@ -52,6 +55,10 @@ const messageTimeLine = (message) => {
   message.reply(
     "Tiết 1: 6.45 -> 7.35\nTiết 2: 7.40 -> 8.30\nTiết 3: 8.40 -> 9.30\nTiết 4: 9.40 -> 10.30\nTiết 5: 10.35 -> 11.25\nTiết 6: 13.30 -> 13.50\nTiết 7: 13.55 -> 14.45\nTiết 8: 14.55 -> 15.45\nTiết 9: 15.55 -> 16.45\nTiết 10: 16.50 -> 17.40\n"
   );
+};
+const waitAndSendMessage = async (message) => {
+  const msg = await message.reply("Chờ mình tí nhé...");
+  msg.edit("");
 };
 module.exports = {
   messageSchedule,
--- src/index.js
+++ src/index.js
@@ -71,3 +71,4 @@ client.on("messageCreate", async (message) => {
     }
   }
 });
+
